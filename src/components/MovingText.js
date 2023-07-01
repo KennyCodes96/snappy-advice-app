af5f9986a2b54e50645cdf1d892feeca07dbbc50
KennyCodes96 <kennethplumstead@icloud.com>
2023-07-01 18:55:19 -0400

    initial commit

---
+++ src/components/MovingText.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './MovingText.css';
+
+const MovingText = () => {
+    return (
+      <div className="moving-text-container">
+        <h1 className="moving-text">Snappy Advice</h1>
+      </div>
+    );
+};
+
+export default MovingText;
